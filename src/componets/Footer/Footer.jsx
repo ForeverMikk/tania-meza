985812b00c2cc2d4f209d5210831891aa4468f23
Michael Molina <mi1mo2e3@gmail.com>
2022-04-26 16:09:27 -0500

    save some things

--- src/componets/Footer/Footer.jsx
+++ src/componets/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 
 import logo from './../../assets/img/tania-meza.png'
 
