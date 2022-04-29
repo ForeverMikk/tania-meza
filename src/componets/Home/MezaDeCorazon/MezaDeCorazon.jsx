7253724da796c13ad653ff9498be1f7d9339467e
Michael Molina <mi1mo2e3@gmail.com>
2022-04-28 20:17:31 -0500

    arreglando estilos en el home y las noticias

--- src/componets/Home/MezaDeCorazon/MezaDeCorazon.jsx
+++ src/componets/Home/MezaDeCorazon/MezaDeCorazon.jsx
@@ -19,13 +19,19 @@ const MezaDeCorazon = () => {
                 </div>
 
                 <div className="video" data-aos='fade-left' data-aos-duration="850">
-                    <iframe src="https://www.youtube.com/embed/_Tx1WpIWdW0" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                    <iframe 
+                        src="https://www.youtube.com/embed/_Tx1WpIWdW0" 
+                        title="YouTube video player" 
+                        frameBorder="0" 
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                        allowFullScreen
+                    ></iframe>
                 </div>
 
                 <div className="media">
                     <a href="https://www.facebook.com/TaniaMezaEscorza?ref=page_internal" target="_blank" rel="noreferrer" className="facebook"><FontAwesomeIcon icon={faFacebook} /></a>
-                    <a href="https://www.instagram.com/taniamezaescorza/" target="_blank" rel="noreferrer" className="instagram"><FontAwesomeIcon icon={faTwitter} /></a>
-                    <a href="https://twitter.com/taniameza_" target="_blank" rel="noreferrer" className="twitter"><FontAwesomeIcon icon={faInstagram} /></a>
+                    <a href="https://twitter.com/taniameza_" target="_blank" rel="noreferrer" className="instagram"><FontAwesomeIcon icon={faTwitter} /></a>
+                    <a href="https://www.instagram.com/taniamezaescorza/" target="_blank" rel="noreferrer" className="twitter"><FontAwesomeIcon icon={faInstagram} /></a>
                 </div>
             </div>     
         </div>
